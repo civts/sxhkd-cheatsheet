a9e297ac673d6577c212c5daecfba2db1b8d34ef
civts <giacomo.zanolli@studenti.unitn.it>
2022-06-12 21:24:00 +0200

    Can display shortcuts, though all in one color

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/event.hpp"
 #include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/dom/node.hpp"
+#include "ftxui/screen/screen.hpp"
 #include "group.cpp"
 #include "shortcut.cpp"
 #include <cstdlib>
@@ -20,16 +23,58 @@ int main(int argc, const char *argv[]) {
     exit(1);
   }
   string path = argv[1];
-  cout << path << endl << endl;
 
   vector<Group> result = extractor(path);
+  vector<Element> left = {};
+  vector<Element> right = {};
+
+  int totalshortcuts = 0;
   for (Group g : result) {
-    cout << ">>>> " << g.name << endl;
-    for (Shortcut s : g.shortcuts) {
-      // cout << "-- " << s.keys << endl;
-      cout << "- " << s.description << endl;
-      // cout << "---- " << s.command << endl;
-    }
+    totalshortcuts += g.shortcuts.size();
   }
-  return 0;
+  int shortcutsUntilNow = 0;
+  bool previousPushWasLeft = true;
+  int halfShortcutsLen = totalshortcuts / 2;
+  for (int i = 0; i < result.size(); i++) {
+    Group g = result[i];
+    vector<Element> children = vector<Element>();
+    bool isFirstLeft = i == 0;
+    bool isFirstRight =
+        previousPushWasLeft && shortcutsUntilNow > halfShortcutsLen;
+    bool isFirstInAColumn = isFirstLeft || isFirstRight;
+    bool shouldAddSpace = !isFirstInAColumn;
+    if (shouldAddSpace) {
+      children.push_back(text(""));
+    }
+    children.push_back(hbox({
+        text(g.name) | bgcolor(Color::Aquamarine1),
+        filler(),
+    }));
+    for (Shortcut s : g.shortcuts) {
+      Element shortcutElem = hbox({
+          text(s.keys) | bold,
+          paragraph(" " + s.description),
+      });
+      children.push_back(shortcutElem);
+    }
+    if (shortcutsUntilNow <= halfShortcutsLen) {
+      left.push_back(vbox(children));
+    } else {
+      right.push_back(vbox(children));
+      previousPushWasLeft = false;
+    }
+    shortcutsUntilNow += g.shortcuts.size();
+  }
+  Element document = hbox({
+      vbox(left),
+      text(" "),
+      vbox(right),
+  });
+
+  Screen screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+
+  std::cout << screen.ToString() << std::endl;
+
+  return EXIT_SUCCESS;
 }
